e7e223b3d09c83d4cd4df6eae09a7196c832f0f1
Владислав <vladislav@MacBook-Pro-Vladislav.local>
2020-04-17 09:09:00 +0300

    Add Unit

--- src/main/java/dao/DictionaryDaoImpl.java
+++ src/main/java/dao/DictionaryDaoImpl.java
@@ -21,13 +21,8 @@ public class DictionaryDaoImpl implements DictionaryDao{
     private static final String GET_AREA = "SELECT * FROM jc_country_struct\n" +
             "WHERE area_id LIKE ? AND area_id <> ?";
 
-    //TODO refactoring - make one method
     private Connection getConnection() throws SQLException {
-        Connection con = DriverManager.getConnection(
-                Config.getProperty(Config.DB_URL),
-                Config.getProperty(Config.DB_LOGIN),
-                Config.getProperty(Config.DB_PASSWORD));
-        return con;
+        return ConnectionBuilder.getConnection();
     }
 
 
